89bb57ae804c2d3cd34f932b8a2f0d2ef4f4ee61
glowingstone124 <hanserofficial@outlook.com>
2024-03-01 16:51:47 +0800

    change default response header to application/json

---
+++ src/main/java/org/qo/WebConfig.java
@@ -0,0 +1,13 @@
+package org.qo;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
