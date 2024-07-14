24ba04d5d0e188970e4d50f8ace95d8792b00455
glowingstone124 <hanserofficial@outlook.com>
2024-07-14 20:30:36 +0800

    pass ci

--- src/main/java/org/qo/PoolUtils.java
+++ src/main/java/org/qo/PoolUtils.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class PoolUtils {
-    public ExecutorService es = Executors.newVirtualThreadPerTaskExecutor();
+    public ExecutorService es = Executors.newCachedThreadPool();
     public void submit(Runnable task) {
         es.submit(task);
     }
