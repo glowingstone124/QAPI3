218241fc03bcc969bfad709d02ccbaac1b2f66f6
glowingstone124 <hanserofficial@outlook.com>
2024-09-03 16:21:58 +0800

    [test] stage 2 for github webhook

--- src/main/java/org/qo/tracks/Controller.kt
+++ src/main/java/org/qo/tracks/Controller.kt
@@ -1,44 +1,55 @@
 package org.qo.tracks
 
-import com.google.gson.JsonObject
 import org.json.JSONArray
 import org.json.JSONObject
 import org.qo.Msg
-import org.qo.ReturnInterface
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @SpringBootApplication
 @RequestMapping("/hooks")
 open class Controller {
+
     @PostMapping("/accept")
     fun accept(@RequestBody obj: String) {
-        val githubEvent: JSONObject = JSONObject(obj)
-        println(githubEvent)
+        val githubEvent = JSONObject(obj)
+        val sb = StringBuilder()
         if (githubEvent.has("action")) {
-            return;
+            if (githubEvent.getString("action") == "completed") {
+                val runResult = githubEvent.getJSONObject("workflow_run")
+                val times = runResult.getInt("run_number")
+                val repository = runResult.getJSONObject("repository").getString("name")
+                val title = runResult.getString("display_title")
+                val status = runResult.getString("status")
+
+                sb.append("===========Github Update===========\n")
+                sb.append("Github Actions触发，以下是详细信息\n")
+                sb.append("$repository 构建 $times\n")
+                sb.append("简介：$title\n")
+                sb.append("运行结果：$status\n")
+
+                Msg.put(sb.toString())
+            }
+            return
         }
-        val repoName =githubEvent.getJSONObject("repository").getString("name")
+
+        val repoName = githubEvent.getJSONObject("repository").getString("name")
         val commitsArr: JSONArray = githubEvent.getJSONArray("commits")
         val sender = githubEvent.getJSONObject("sender").getString("login")
-        val sb:StringBuilder = StringBuilder()
-        sb.append("===========Github Update===========")
-        sb.append("用户：$sender 上传了 ${commitsArr.length()} 个commit到仓库 $repoName")
-        sb.append("--------------Summery--------------")
+
+        sb.append("===========Github Update===========\n")
+        sb.append("用户：$sender 上传了 ${commitsArr.length()} 个 commit 到仓库 $repoName\n")
+        sb.append("--------------Summary--------------\n")
+
         commitsArr.forEach {
             it as JSONObject
             val msg = it.getString("message")
             val author = it.getJSONObject("author").getString("username")
-            sb.append("作者：$author")
-            sb.append("说明: $msg")
-            sb.append("-----------------------------------")
+
+            sb.append("作者：$author\n")
+            sb.append("说明: $msg\n")
+            sb.append("-----------------------------------\n")
         }
         Msg.put(sb.toString())
     }
