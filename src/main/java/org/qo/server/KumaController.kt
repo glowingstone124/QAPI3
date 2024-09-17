71bb30b208ad4dcff237d88fafc1bcae162f1567
glowingstone124 <hanserofficial@outlook.com>
2024-09-17 20:12:29 +0800

    Add KumaController and KumaService for handling uploads
    Introduced `KumaController` to manage upload-related endpoints and `KumaService` to handle the logic for processing these uploads. Added methods for configuration initialization and parsing webhook requests within `KumaService`.

---
+++ src/main/java/org/qo/server/KumaController.kt
@@ -0,0 +1,19 @@
+package org.qo.server
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class KumaController {
+    @Autowired
+    lateinit var kumaService: KumaService
+
+    @PostMapping("/kuma/upload")
+    fun handleKumaUpload(@RequestBody input: String, request: HttpServletRequest): ResponseEntity<String> {
+        return kumaService.handleMessage(input, request)
+    }
+}
