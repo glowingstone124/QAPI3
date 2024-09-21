ae0ff7782dd9bccab1afd9e0fd78f261bba586ca
glowingstone124 <hanserofficial@outlook.com>
2024-09-21 11:42:11 +0800

    test ci

--- src/main/java/org/qo/mail/MailPreset.java
+++ src/main/java/org/qo/mail/MailPreset.java
@@ -4,6 +4,6 @@ public class MailPreset {
     public static String register = """
             <h1>Quantum Original2注册</h1>
             <h2>您的此QQ账号刚刚注册了QO2，欢迎您游玩QO2。如果您有任何问题，欢迎询问1294915648</h2>
-            <p>Powered by QAPI3</p>
+            <p>Powered by QAPI3.5</p>
             """;
 }
