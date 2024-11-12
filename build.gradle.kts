664a5bd7bfc6e8844f8feacbe7b180c41a3527c1
glowingstone124 <hanserofficial@outlook.com>
2024-11-12 09:00:17 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
