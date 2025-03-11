4d1bb532ca9ceb91d6501e2323395c631a9e5583
glowingstone124 <hanserofficial@outlook.com>
2025-03-11 18:55:21 +0800

    make hashValue's output always positive

--- src/main/java/org/qo/loginService/AuthorityNeededServicesImpl.kt
+++ src/main/java/org/qo/loginService/AuthorityNeededServicesImpl.kt
@@ -139,12 +139,13 @@ class FortuneTools {
 	fun hashValue(userId: String): Triple<Int, Int, Int> {
 		val md5 = MessageDigest.getInstance("MD5").digest(userId.toByteArray()).joinToString("") { "%02x".format(it) }
 
-		val base1 = (md5.substring(0, 8).toLong(16) and 0xFFFFFFFF).toInt() % 100
-		val base2 = (md5.substring(8, 16).toLong(16) and 0xFFFFFFFF).toInt() % 100
-		val base3 = (md5.substring(16, 24).toLong(16) and 0xFFFFFFFF).toInt() % 100
+		val base1 = (md5.substring(0, 8).toLong(16) % 100).toInt()
+		val base2 = (md5.substring(8, 16).toLong(16) % 100).toInt()
+		val base3 = (md5.substring(16, 24).toLong(16) % 100).toInt()
 
 		return Triple(base1, base2, base3)
 	}
+
 
 
 	fun calculateFortune(userId: String): Fortune {
