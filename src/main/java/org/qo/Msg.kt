8654f57f7d8242ab8dc5e1121f385a832fde5f41
glowingstone124 <hanserofficial@outlook.com>
2025-03-15 21:08:47 +0800

    refactor Messages, prepare moving to SQL for Data Persistence

--- src/main/java/org/qo/Msg.kt
+++ src/main/java/org/qo/Msg.kt
@@ -1,39 +1,36 @@
 package org.qo
 
+import com.google.gson.Gson
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import kotlinx.coroutines.*
 import org.qo.loginService.Login
-import org.qo.customAnnotations.Authority
-import org.qo.loginService.AuthorityNeededServicesImpl
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import java.io.FileWriter
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.LinkedBlockingQueue
+
 @Service
 class Msg {
     companion object {
         const val MAX_QUEUE_SIZE = 300
-        val msgQueue = LinkedBlockingQueue<String>(MAX_QUEUE_SIZE)
+        val msgQueue = LinkedBlockingQueue<Message>(MAX_QUEUE_SIZE)
+        val gson = Gson()
+
         val login = Login()
+
         suspend fun sse(): SseEmitter {
             val emitter = SseEmitter(0L)
             val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
             scope.launch {
                 try {
-                    if (!msgQueue.isEmpty()) {
-                        val jsonArr = msgQueue.take()
-                        emitter.send(JsonObject().apply {
-                            addProperty("messages", jsonArr)
-                        }, MediaType.APPLICATION_JSON)
-                    }
                     while (true) {
-                        val jsonArr = msgQueue.take()
+                        val message = msgQueue.take()
                         emitter.send(JsonObject().apply {
-                            addProperty("messages", jsonArr)
+                            addProperty("messages", gson.toJson(message))
                         }, MediaType.APPLICATION_JSON)
                     }
                 } catch (e: Exception) {
@@ -49,58 +46,61 @@ class Msg {
                     addProperty("code", 400)
                 } else {
                     addProperty("code", 0)
-                    addProperty("content", msgQueue.peek())
+                    addProperty("content", gson.toJson(msgQueue))
                 }
             }.toString()
         }
+
         fun put(msg: String) {
             FileWriter("chathistory.txt", StandardCharsets.UTF_8).use { writer ->
                 writer.write(msg)
                 if (msgQueue.remainingCapacity() == 0) {
                     msgQueue.poll()
                 }
-                msgQueue.offer(msg)
+                msgQueue.offer(Message(msg, 1, "Sender", System.currentTimeMillis()))
             }
         }
+
         fun putSys(msg: String) {
-            val msgObj = JsonObject().apply {
-                addProperty("message", msg)
-                addProperty("from", 2)
-                addProperty("sender", "System")
-                addProperty("time", System.currentTimeMillis())
-            }
+            val msgObj = Message(msg, 2, "System", System.currentTimeMillis())
             FileWriter("chathistory.txt", StandardCharsets.UTF_8).use { writer ->
-                writer.write(msgObj.toString())
-                generalPut(msgObj.toString())
+                writer.write(gson.toJson(msgObj))
+                generalPut(msgObj)
             }
         }
-        fun generalPut(msg: String) {
+
+        fun generalPut(msg: Message) {
             if (msgQueue.remainingCapacity() == 0) {
                 msgQueue.poll()
             }
             msgQueue.offer(msg)
         }
-        fun putWebchat(msg:String, sender:String) {
-            val msgObj = JsonObject().apply {
-                addProperty("message", msg)
-                addProperty("from", 3)
-                addProperty("sender", "<Web>$sender")
-                addProperty("time", System.currentTimeMillis())
-            }
-            generalPut(msgObj.toString())
+
+        fun putWebchat(msg: String, sender: String) {
+            val msgObj = Message(msg, 3, "<Web>$sender", System.currentTimeMillis())
+            generalPut(msgObj)
         }
+
         fun get(): JsonObject {
             return JsonObject().apply {
                 add("messages", msgQueue.toJsonArray())
                 addProperty("empty", msgQueue.isEmpty())
             }
         }
+
         fun <T> LinkedBlockingQueue<T>.toJsonArray(): JsonArray {
             val arr = JsonArray()
             this.forEach { item ->
-                arr.add(item.toString())
+                arr.add(gson.toJson(item))
             }
             return arr
         }
     }
 }
+
+data class Message(
+    val message: String,
+    val from: Int,
+    val sender: String,
+    val time: Long
+)
