193b6baa8e70673c50aac2ccfa06e3929eeed6bd
glowingstone124 <hanserofficial@outlook.com>
2025-05-17 16:19:37 +0800

    small fix

--- src/main/java/org/qo/services/metroServices/MetroServiceController.kt
+++ src/main/java/org/qo/services/metroServices/MetroServiceController.kt
@@ -1,12 +1,14 @@
 package org.qo.services.metroServices
 
+import org.qo.utils.ReturnInterface
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 @RestController
-class MetroServiceController(private val serviceImpl: MetroServiceImpl) {
+class MetroServiceController(private val serviceImpl: MetroServiceImpl, private val ri: ReturnInterface) {
 	@GetMapping("/qo/metro/download")
-	fun downloadMetro(): String {
-		return serviceImpl.getMetroJson()
+	fun downloadMetro(): ResponseEntity<String> {
+		return ri.GeneralHttpHeader(serviceImpl.getMetroJson())
 	}
 
 }
