4d29af43372d4f1b5077bbdef87686e3b7b17bef
glowingstone124 <hanserofficial@outlook.com>
2025-06-01 16:35:23 +0800

    debug

--- src/main/java/org/qo/services/llmServices/LLMController.kt
+++ src/main/java/org/qo/services/llmServices/LLMController.kt
@@ -1,27 +1,19 @@
 package org.qo.services.llmServices
 
-import kotlinx.coroutines.reactor.asFlux
+import kotlinx.coroutines.flow.Flow
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
-import reactor.core.publisher.Flux
 
 @RestController
 @RequestMapping("/qo/asking/")
 class LLMController(private val llmServices: LLMServices) {
-
 	@PostMapping("/ask", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-	fun handleResponse(
-		@RequestHeader("Authorization") requestToken: String,
-		@RequestBody body: String
-	): Flux<String> {
-		return kotlinx.coroutines.reactor.mono {
-			val result = llmServices.generateLLMStream(body, requestToken)
-			if (!result.second) {
-				Flux.just("Unpermitted access")
-			} else {
-				result.first!!.asFlux()
-			}
-		}.flatMapMany { it }
+	suspend fun handleResponse(@RequestHeader("Authorization") requestToken: String, @RequestBody body: String): Flow<String>? {
+		val result = llmServices.generateLLMStream(body, requestToken)
+		return if (!result.second) {
+			null
+		} else {
+			result.first
+		}
 	}
 }
-
