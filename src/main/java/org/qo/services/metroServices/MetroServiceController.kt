d58df3e0fb7cbd18bb329ff751ba7136b05d6250
glowingstone124 <hanserofficial@outlook.com>
2025-06-03 21:37:49 +0800

    add segment create

--- src/main/java/org/qo/services/metroServices/MetroServiceController.kt
+++ src/main/java/org/qo/services/metroServices/MetroServiceController.kt
@@ -1,14 +1,22 @@
 package org.qo.services.metroServices
 
+import org.qo.datas.Nodes
 import org.qo.utils.ReturnInterface
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+
 @RestController
-class MetroServiceController(private val serviceImpl: MetroServiceImpl, private val ri: ReturnInterface) {
+class MetroServiceController(private val serviceImpl: MetroServiceImpl, private val ri: ReturnInterface, private val nodes: Nodes) {
 	@GetMapping("/qo/metro/download")
 	fun downloadMetro(): ResponseEntity<String> {
 		return ri.GeneralHttpHeader(serviceImpl.getMetroJson())
 	}
-
+	@PostMapping("/qo/metro/upload")
+	fun uploadMetro(@RequestBody data: String, @RequestHeader("Token") token: String): ResponseEntity<String> {
+		return ri.GeneralHttpHeader(serviceImpl.preInsertCheck(data, token))
+	}
 }
--- src/main/java/org/qo/services/metroServices/MetroServiceImpl.kt
+++ src/main/java/org/qo/services/metroServices/MetroServiceImpl.kt
@@ -4,13 +4,13 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
-import com.google.gson.reflect.TypeToken
 import org.qo.datas.ConnectionPool
-import org.qo.orm.SQL
+import org.qo.datas.Nodes
 import org.springframework.stereotype.Service
 
 @Service
-class MetroServiceImpl {
+class MetroServiceImpl(private val nodes:Nodes) {
+	val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 	data class Signal(
 		val world: String,
 		val x: Int,
@@ -24,9 +24,17 @@ class MetroServiceImpl {
 		val dummy: String,
 		val signal: List<JsonObject>
 	)
+	data class SectionWithId(
+		val id: String,
+		val lid: Int,
+		val station: Boolean,
+		val dummy: String,
+		val signal: List<JsonObject>,
+		val author: Long
+	)
+
 
 	fun getMetroJson(): String {
-		val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 
 		val sectionMap = mutableMapOf<String, Section>()
 		val connection = ConnectionPool.getConnection()
@@ -61,4 +69,60 @@ class MetroServiceImpl {
 
 		return gson.toJson(sectionMap)
 	}
+
+
+	fun preInsertCheck(body: String, token: String): String {
+		if (nodes.getServerFromToken(token) < 0){
+			return "Err: Not legal token"
+		}
+		val section = gson.fromJson(body, SectionWithId::class.java)
+
+		val signalUp = section.signal.getOrNull(0)
+		val signalDown = section.signal.getOrNull(1)
+
+		val result = insertSection(
+			id = section.id,
+			lid = section.lid,
+			station = section.station,
+			dummy = section.dummy,
+			signalUp = signalUp,
+			signalDown = signalDown,
+			author = section.author
+		)
+		if (result){
+			return "OK"
+		}
+		return "Err: Not valid json"
+	}
+	/**
+	 * @param id 十六进制id
+	 * @param lid 线路编号
+	 * @param station 站点编号
+	 * @param dummy 名字
+	 * @param signalUp 上行地标位置
+	 * @param signalDown 下行地标位置
+	 * @param author 来源
+	 */
+	fun insertSection(id: String, lid: Int, station: Boolean, dummy: String, signalUp: JsonObject?, signalDown: JsonObject?, author: Long): Boolean {
+
+		val connection = ConnectionPool.getConnection()
+		val sql = """
+		INSERT INTO sections (id, lid, station, dummy, signal_up, signal_down, author)
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+	""".trimIndent()
+
+		connection.use { conn ->
+			conn.prepareStatement(sql).use { stmt ->
+				stmt.setString(1, id)
+				stmt.setInt(2, lid)
+				stmt.setBoolean(3, station)
+				stmt.setString(4, dummy)
+				stmt.setString(5, signalUp?.toString())
+				stmt.setString(6, signalDown?.toString())
+				stmt.setString(7, author.toString())
+				return stmt.executeUpdate() > 0
+			}
+		}
+	}
+
 }
