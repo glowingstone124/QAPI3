8f51d84e89f4238ade03eaeefbcbb0dc5b0bf1b1
glowingstone124 <hanserofficial@outlook.com>
2025-07-16 17:58:04 +0800

    automatically renew mmdb

--- src/main/java/org/qo/services/mmdb/Init.kt
+++ src/main/java/org/qo/services/mmdb/Init.kt
@@ -4,9 +4,15 @@ import com.maxmind.geoip2.DatabaseReader
 import org.qo.utils.Logger
 import org.qo.utils.Logger.LogLevel.*
 import org.qo.utils.Request
+import org.springframework.scheduling.annotation.Scheduled
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
+import java.time.Duration
+import java.time.Instant
+import kotlin.io.path.deleteExisting
 import kotlin.io.path.exists
 
 object Init {
@@ -20,14 +26,37 @@ object Init {
 		if (!Path.of(geoip_db_local).exists()) {
 			Logger.log("NO local GeoIP database found.", INFO)
 			Logger.log("Downloading...", INFO)
-
-			Request().download(geoip_db_url, geoip_db_local).completeAsync {
-				Logger.log("Download complete.", INFO)
-				initializeDatabase()
-			}
+			downloadMmdb()
 		} else {
 			initializeDatabase()
 		}
+		renewMmdb()
+	}
+
+	private fun downloadMmdb() {
+		Request().download(geoip_db_url, geoip_db_local).completeAsync {
+			Logger.log("Download complete.", INFO)
+			initializeDatabase()
+		}
+	}
+	@Scheduled(fixedRate = 60 * 60 * 1000)
+	private fun renewMmdb() {
+		val path = File(geoip_db_local).toPath()
+
+		if (!Files.exists(path)) {
+			downloadMmdb()
+			return
+		}
+
+		val attrs = Files.readAttributes(path, BasicFileAttributes::class.java)
+		val hoursSinceLastModified = Duration.between(attrs.lastModifiedTime().toInstant(), Instant.now()).toHours()
+
+		if (hoursSinceLastModified >= 72) {
+			Files.deleteIfExists(path)
+			downloadMmdb()
+		}
+
+		Logger.log("Renewed mmdb database.", INFO)
 	}
 
 	private fun initializeDatabase() {
