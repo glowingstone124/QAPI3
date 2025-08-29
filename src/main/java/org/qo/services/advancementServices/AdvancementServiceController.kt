b207498fa120017ebd84656dc9a7120d251f25a1
glowingstone124 <hanserofficial@outlook.com>
2025-08-29 19:04:11 +0800

    fix

--- src/main/java/org/qo/services/advancementServices/AdvancementServiceController.kt
+++ src/main/java/org/qo/services/advancementServices/AdvancementServiceController.kt
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/qo/advancement")
+@RestController
+@RequestMapping("/qo/advancement")
 class AdvancementServiceController(
 	private val advancementServiceImpl: AdvancementServiceImpl,
 	private val nodes: Nodes
