203ab82f220c2cc39518991d65c7ae0238b134b1
glowingstone124 <hanserofficial@outlook.com>
2026-02-04 09:46:04 +0800

    fix

--- src/main/java/org/qo/services/transportationServices/TransportationServiceController.kt
+++ src/main/java/org/qo/services/transportationServices/TransportationServiceController.kt
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity
 import org.springframework.util.RouteMatcher
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/qo/transportation")
+@RestController
+@RequestMapping("/qo/transportation")
 class TransportationServiceController(
 	private val service: TransportationServiceImpl
 ) {
